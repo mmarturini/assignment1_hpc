6de03a700766e1787be25887eedec48b0f760e13
mmarturi <m.marturini22@gmail.com>
2021-12-28 14:42:07 +0100

    section 2

--- thin_jacobi_cores/launch_inside_socket.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load openmpi/4.0.3/gnu/9.3.0
-mpif77 -ffixed-line-length-none Jacobi_MPI_vectormode.F -o jacoby3D.x
-
-mpirun --mca btl ^openib --map-by core -np 4 ./jacoby3D.x <input.1200 >thin_inside_socket_4.out
-mpirun --mca btl ^openib --map-by core -np 8 ./jacoby3D.x <input.1200 >thin_inside_socket_8.out
-mpirun --mca btl ^openib --map-by core -np 12 ./jacoby3D.x <input.1200 >thin_inside_socket_12.out
-
--- thin_jacobi_sockets/launch_between_sockets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mpirun --mca btl ^openib --map-by socket -np 4 ./jacoby3D.x <input.1200 >thin_between_sockets_4.out
-mpirun --mca btl ^openib --map-by socket -np 8 ./jacoby3D.x <input.1200 >thin_between_sockets_8.out
-mpirun --mca btl ^openib --map-by socket -np 12 ./jacoby3D.x <input.1200 >thin_between_sockets_12.out
-
